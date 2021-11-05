2238bc80a9ef24ee522da251c32b0e25710f71be
Victor Aguilar C <vicobits@gmail.com>
2021-11-05 12:44:25 -0600

    fix: update dependencies

--- setup.py
+++ setup.py
@@ -33,6 +33,7 @@ with open('README.rst') as readme:
               'Programming Language :: Python :: 3.3',
               'Programming Language :: Python :: 3.4',
               'Programming Language :: Python :: 3.5',
+              'Programming Language :: Python :: 3.7',
               'Topic :: Software Development :: Libraries :: Python Modules',
               'Topic :: Software Development :: Quality Assurance'],
           keywords='beautify, code, format, formatter, reformat, style',
